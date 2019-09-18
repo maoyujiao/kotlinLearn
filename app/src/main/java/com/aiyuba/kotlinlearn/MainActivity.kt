a505588e85a444364b01e0a20dbd7faa205e1d9e
maoyujiao <2357614872@qq.com>
2019-09-18 13:59:22 +0800

    Initial commit

---
+++ app/src/main/java/com/aiyuba/kotlinlearn/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.aiyuba.kotlinlearn
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        txt.setText(R.string.hello_world)
+    }
+
+}
